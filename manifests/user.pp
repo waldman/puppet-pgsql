7b451c29be3e9d04ec340a6da378d76fb00aa9fe
Leon Waldman <le.waldman@gmail.com>
2012-06-24 01:44:39 +0300

    Small cosmetic fix

--- manifests/user.pp
+++ manifests/user.pp
@@ -14,7 +14,6 @@
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 ###
 
-
 # Inspired by the excellent pgsql puppet modules from Eivind Uggedal
 # (puppet-module-postgresql) and Luke Kanies (puppetlabs-postgres)
 # 
